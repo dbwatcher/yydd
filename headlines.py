99dd3462616d34ef139aa269363211ee01d2403b
dbwatcher <dbwatcher@163.com>
2018-02-12 04:03:52 +0800

    v0.2 commit.

--- headlines.py
+++ headlines.py
@@ -1,9 +1,25 @@
 
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import feedparser
 from flask import Flask
 app = Flask(__name__)
-
+ZHIHU_FEED = "https://www.zhihu.com/rss"
 @app.route("/")
 def get_news():
-  return "no news is good news"
-if __name__ == '__main__':
-  app.run(host='0.0.0.0',port=5000, debug=True)
+     feed = feedparser.parse(ZHIHU_FEED)
+     first_article = feed['entries'][0]
+     return """<html>
+       <body>
+           <h1> zhihu Headlines </h1>
+           <b>{0}</b> <br/>
+           <i>{1}</i> <br/>
+           <p>{2}</p> <br/>
+       </body>
+   </html>""".format(first_article.get("title"), first_article.
+   get("published"), first_article.get("summary"))
+if __name__ == "__main__":
+     app.run(host='0.0.0.0', port=5000, debug=True)
