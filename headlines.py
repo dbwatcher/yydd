70d131d613b034c800ff57fb847328aa7a0b1c6e
dbwatcher <dbwatcher@flask.(none)>
2018-02-12 06:24:05 +0800

    v0.3

--- headlines.py
+++ headlines.py
@@ -1,5 +1,3 @@
-
-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -7,19 +5,34 @@ sys.setdefaultencoding('utf-8')
 import feedparser
 from flask import Flask
 app = Flask(__name__)
-ZHIHU_FEED = "https://www.zhihu.com/rss"
+
+RSS_FEED = {"zhihu": "https://www.zhihu.com/rss",
+            "netease": "http://news.163.com/special/00011K6L/rss_newsattitude.xml",
+            "songshuhui": "http://songshuhui.net/feed",
+            "ifeng": "http://news.ifeng.com/rss"}
+
 @app.route("/")
-def get_news():
-     feed = feedparser.parse(ZHIHU_FEED)
-     first_article = feed['entries'][0]
-     return """<html>
+@app.route("/zhihu")
+def zhihu():
+    return get_news('zhihu')
+@app.route("/netease")
+def netease():
+    return get_news('netease')
+@app.route("/songshuhui")
+def ifeng():
+    return get_news('songshuhui')
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEED[publication])
+    first_article = feed['entries'][0]
+    return """<html>
        <body>
-           <h1> zhihu Headlines </h1>
+           <h1> news Headlines </h1>
            <b>{0}</b> <br/>
            <i>{1}</i> <br/>
            <p>{2}</p> <br/>
        </body>
    </html>""".format(first_article.get("title"), first_article.
    get("published"), first_article.get("summary"))
+
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=5000, debug=True)
