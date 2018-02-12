9a205266b273290f9a6acc0cd51c78e18638e0e9
dbwatcher <dbwatcher@163.com>
2018-02-12 08:52:17 +0800

    with Jinja templates

--- headlines.py
+++ headlines.py
@@ -3,36 +3,19 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 RSS_FEED = {"zhihu": "https://www.zhihu.com/rss",
             "netease": "http://news.163.com/special/00011K6L/rss_newsattitude.xml",
             "songshuhui": "http://songshuhui.net/feed",
-            "ifeng": "http://news.ifeng.com/rss"}
+            "ifeng": "http://news.ifeng.com/rss/index.xml"}
 
 @app.route("/")
-@app.route("/zhihu")
-def zhihu():
-    return get_news('zhihu')
-@app.route("/netease")
-def netease():
-    return get_news('netease')
-@app.route("/songshuhui")
-def ifeng():
-    return get_news('songshuhui')
-def get_news(publication):
+@app.route("/<publication>")
+def get_news(publication="zhihu"):
     feed = feedparser.parse(RSS_FEED[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-       <body>
-           <h1> news Headlines </h1>
-           <b>{0}</b> <br/>
-           <i>{1}</i> <br/>
-           <p>{2}</p> <br/>
-       </body>
-   </html>""".format(first_article.get("title"), first_article.
-   get("published"), first_article.get("summary"))
+    return render_template("home.html", articles=feed['entries'])
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=5000, debug=True)
